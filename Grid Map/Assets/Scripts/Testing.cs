e89979dd587e0a79157d911c47224ce7e0173f6a
pikachuchew <96628283+pikachuchew@users.noreply.github.com>
2022-02-01 00:19:17 -0500

    refrech

---
+++ Grid Map/Assets/Scripts/Testing.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Grid grid = new Grid(1, 2, 10f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
